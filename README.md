f56a0c08afa3d10dffcdc93448765e9091ccfaed
xieshangyu <2817476744@qq.com>
2022-02-22 03:57:12 -0500

    Add existing file

--- README.md
+++
@@ -1,8 +0,0 @@
-# xieshangyu.github.io
-
-
-[week1 assignment1](https://xieshangyu.github.io/week1/index.html)
-
-
-[week2 assignment2](https://xieshangyu.github.io/week2/see.html)
-
---
+++ week3/sketch.js
@@ -0,0 +1,29 @@
+var canvas;
+var mic;
+
+function windowResized() {
+  //console.log('resized');
+  resizeCanvas(windowWidth, windowHeight);
+}
+
+function setup() {
+  canvas = createCanvas(windowWidth, windowHeight);
+  canvas.position(0, 0);
+  canvas.style('z-index', '-1');
+  mic = new p5.AudioIn();
+  mic.start();
+  //background(175);
+}
+
+// function keyPressed() {
+//   clear();
+// }
+
+function draw() {
+//   if (mouseIsPressed) {
+//     line(pmouseX, pmouseY, mouseX, mouseY);
+//   }
+    background(175)
+    var vol = mic.getLevel();
+    ellipse(width / 2, height / 2, vol * 500);
+}
