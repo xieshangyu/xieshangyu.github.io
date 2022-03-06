82370841b222d0cfbcda062cf109d3aacc1196f5
xieshangyu <2817476744@qq.com>
2022-03-06 04:50:16 -0500

    change the menu

--- week5/js/script.js
+++ week5/js/script.js
@@ -1,13 +1,72 @@
 gsap.registerPlugin(ScrollTrigger);
 
 gsap.to(".square", {
-    // x:700,
-    duration: 3,
+    x: 0,
+    duration: 8,
     scrollTrigger: {
-        trigger: ".square",
-        start: "top 30%",
-        end: "center 20%",
-        markers: true,
-        toggleClass: "red"
+        trigger: ".square_2",
+        start: "top 50%",
+        end: "top 20%",
+        scrub: 4,
+        toggleActions: "restart reverse         none           none",
+        //             onEnter onLeave       onEnterback    onleaveBack
+        markers: {
+            startColor: "purple",
+            endColor: "fuchsia",
+        },
+    }
+})
+
+
+gsap.to(".square_2", {
+    x: 100,
+    duration: 8,
+    scrollTrigger: {
+        trigger: ".square_2",
+        start: "top 50%",
+        end: "top 20%",
+        scrub: 4,
+        toggleActions: "restart reverse         none           none",
+        //             onEnter onLeave       onEnterback    onleaveBack
+        markers: {
+            startColor: "green",
+            endColor: "black",
+        },
+    }
+})
+
+
+gsap.to(".square_3", {
+    x: 0,
+    duration: 8,
+    scrollTrigger: {
+        trigger: ".square_2",
+        start: "top 50%",
+        end: "top 20%",
+        scrub: 4,
+        toggleActions: "restart reverse         none           none",
+        //             onEnter onLeave       onEnterback    onleaveBack
+        markers: {
+            startColor: "gold",
+            endColor: "white",
+        },
+    }
+})
+
+
+gsap.to(".square_4", {
+    x: 100,
+    duration: 8,
+    scrollTrigger: {
+        trigger: ".square_2",
+        start: "top 50%",
+        end: "top 20%",
+        scrub: 4,
+        toggleActions: "restart reverse         none           none",
+        //             onEnter onLeave       onEnterback    onleaveBack
+        markers: {
+            startColor: "yellow",
+            endColor: "purple",
+        },
     }
 })
