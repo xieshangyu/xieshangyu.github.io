70399f559b139e313766f2e25feac05c12064800
xieshangyu <2817476744@qq.com>
2022-03-08 03:43:35 -0500

    change the menu

--- week5/js/script.js
+++ week5/js/script.js
@@ -10,10 +10,10 @@ gsap.to(".square", {
         scrub: 4,
         toggleActions: "restart reverse         none           none",
         //             onEnter onLeave       onEnterback    onleaveBack
-        markers: {
-            startColor: "purple",
-            endColor: "fuchsia",
-        },
+        // markers: {
+        //     startColor: "purple",
+        //     endColor: "fuchsia",
+        // },
     }
 })
 
@@ -28,10 +28,10 @@ gsap.to(".square_2", {
         scrub: 4,
         toggleActions: "restart reverse         none           none",
         //             onEnter onLeave       onEnterback    onleaveBack
-        markers: {
-            startColor: "green",
-            endColor: "black",
-        },
+        // markers: {
+        //     startColor: "green",
+        //     endColor: "black",
+        // },
     }
 })
 
@@ -46,10 +46,10 @@ gsap.to(".square_3", {
         scrub: 4,
         toggleActions: "restart reverse         none           none",
         //             onEnter onLeave       onEnterback    onleaveBack
-        markers: {
-            startColor: "gold",
-            endColor: "white",
-        },
+        // markers: {
+        //     startColor: "gold",
+        //     endColor: "white",
+        // },
     }
 })
 
@@ -64,9 +64,60 @@ gsap.to(".square_4", {
         scrub: 4,
         toggleActions: "restart reverse         none           none",
         //             onEnter onLeave       onEnterback    onleaveBack
-        markers: {
-            startColor: "yellow",
-            endColor: "purple",
-        },
+        // markers: {
+        //     startColor: "yellow",
+        //     endColor: "purple",
+        // },
     }
 })
+
+
+
+function openNav() {
+    document.getElementById("mySidepanel").style.width = '50%';
+}
+
+/* Set the width of the sidebar to 0 (hide it) */
+function closeNav() {
+    document.getElementById("mySidepanel").style.width = "0";
+}
+
+
+function openNav_2() {
+    document.getElementById("mySidepanel_2").style.width = '50%';
+}
+
+/* Set the width of the sidebar to 0 (hide it) */
+function closeNav_2() {
+    document.getElementById("mySidepanel_2").style.width = "0";
+}
+
+
+function openNav_3() {
+    document.getElementById("mySidepanel_3").style.width = '50%';
+}
+
+/* Set the width of the sidebar to 0 (hide it) */
+function closeNav_3() {
+    document.getElementById("mySidepanel_3").style.width = "0";
+}
+
+
+function openNav_4() {
+    document.getElementById("mySidepanel_4").style.width = '50%';
+}
+
+/* Set the width of the sidebar to 0 (hide it) */
+function closeNav_4() {
+    document.getElementById("mySidepanel_4").style.width = "0";
+}
+
+
+function openNav_5() {
+    document.getElementById("mySidepanel_5").style.width = '50%';
+}
+
+/* Set the width of the sidebar to 0 (hide it) */
+function closeNav_5() {
+    document.getElementById("mySidepanel_5").style.width = "0";
+}
