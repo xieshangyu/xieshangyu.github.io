27ac428792e0907152ddf091ce534febe82e1325
xieshangyu <2817476744@qq.com>
2022-03-08 04:21:43 -0500

    change the menu

--- week3/sketch.js
+++ week3/sketch.js
@@ -1,29 +1,4 @@
 var canvas;
-// var mic;
-
-// function windowResized() {
-//     //console.log('resized');
-//     resizeCanvas(windowWidth, windowHeight);
-// }
-
-// function setup() {
-//     canvas = createCanvas(windowWidth, windowHeight);
-//     canvas.position(0, 0);
-//     canvas.style('z-index', '-1');
-//     mic = new p5.AudioIn();
-//     mic.start();
-//     //background(175);
-// }
-
-// // function keyPressed() {
-// //   clear();
-// // }
-
-// function draw() {
-//     background(175);
-//     var vol = mic.getLevel();
-//     ellipse(width / 2, height / 2, vol * 10000);
-// }
 
 let angle = 0;
 
